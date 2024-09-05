8dc1b166cac8889387b864bb647912e529a7d315
HuanLe <admin@duchuanblog.com>
2024-09-05 21:13:37 +0700

    create file new in theme

---
+++ archive.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<?php if (have_posts()) : ?>
+	<?php while (have_posts()) : the_post(); ?>
+    <?php the_content_rss('',false,'',50);?>
+    <?php endwhile; ?>
+<?php endif; ?>
+<?php get_footer(); ?>
