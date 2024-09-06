65c5901457750d938b4939551c5e79b51df505ec
Picacao <found@duchuanblog.com>
2024-09-06 16:10:21 +0700

    change style page

--- page.php
+++ page.php
@@ -8,7 +8,7 @@ get_header(); ?>
 <div class="row">
     <div class="col-md-12">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <h1><?php the_title(); ?></h1>
+    <h1 class="display-1 pb-5 pt-5"><?php the_title(); ?></h1>
     <?php the_content(); ?>
     <?php endwhile; endif; ?>
     </div>
