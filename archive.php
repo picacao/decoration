1441d062d02af7d791de9d4285674d3f8cf0ed45
Picacao <found@duchuanblog.com>
2024-09-07 09:59:14 +0700

    put get name the_category

--- archive.php
+++ archive.php
@@ -14,7 +14,7 @@
                       </div>
                       <div class="post-content">
                         <div class="post-meta text-uppercase py-3">
-                          <span class="post-category">Furniture</span> / <span class="meta-date"><?php the_date(); ?></span>
+                          <span class="post-category"><?php get_the_category();?></span> / <span class="meta-date"><?php the_date(); ?></span>
                         </div>
                         <h5 class="post-title text-uppercase">
                           <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
