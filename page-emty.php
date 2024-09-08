65060d8ad0513639399eb5b232159a1b406f743d
Picacao <found@duchuanblog.com>
2024-09-08 15:34:26 +0700

    edit code page

--- page-emty.php
+++ page-emty.php
@@ -1,11 +1,17 @@
 <?php 
 /*
-Template Name: Page No Title
+Template Name: Page Emty
 */
 ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-<?php the_content(); ?>
-<?php endwhile; endif; ?>
+<div class="container pt-5 pb-5">
+<div class="row">
+    <div class="col-md-12">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    
+    <?php the_content(); ?>
+    <?php endwhile; endif; ?>
+    </div>
+</div>
+</div>
 
--- page-notitle.php
+++ page-notitle.php
@@ -4,9 +4,16 @@ Template Name: Page No Title
 */
 get_header(); ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<?php the_title(); ?>
-<?php the_content(); ?>
-<?php endwhile; endif; ?>
+<div class="container pt-5 pb-5">
+<div class="row">
+    <div class="col-md-12">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    
+    <?php the_content(); ?>
+    <?php endwhile; endif; ?>
+    </div>
+</div>
+</div>
+
 
 <?php get_footer(); ?>
