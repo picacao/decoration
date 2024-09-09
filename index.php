2cfd799a2176447d52a41f61c9cb2a9ea89c34f3
Picacao <found@duchuanblog.com>
2024-09-09 10:08:19 +0700

    update link

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <section id="billboard" class="overflow-hidden style2">
     <div class="banner-item"
-      style="background-image: url(<?php bloginfo('template_url');?>/static/images/banner-image.jpg); background-repeat: no-repeat; width: 100%; height: 682px; background-position: center; display: flex; align-items: center;">
+      style="background-image: url(<?php bloginfo('template_url');?>/static/img/banner-image.jpg); background-repeat: no-repeat; width: 100%; height: 682px; background-position: center; display: flex; align-items: center;">
       <div class="banner-content padding-large text-center mx-auto">
         <h1 class="display-1 text-uppercase text-light pb-2">wooden table set</h1>
         <p class="mx-auto text-light">Aliquet donec ut arcu risus amet mattis diam gravida. Ac vestibulum quis proin in
