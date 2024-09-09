d0ea37716bec8c814fc284f462e93d2f4f7c750a
Picacao <found@duchuanblog.com>
2024-09-09 10:06:03 +0700

    404

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@
             <p class="lead">
                 The page you’re looking for doesn’t exist.
             </p>
-            <a href="index.html" class="btn btn-primary">Go Home</a>
+            <a href="<?php echo site_url();?>" class="btn btn-primary">Go Home</a>
         </div>
 </div>
 <?php get_footer(); ?>
